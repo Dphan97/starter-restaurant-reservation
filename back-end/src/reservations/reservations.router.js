3afffa4df1cad0b53192b3f2753343b432726b78
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2023-01-09 23:17:25 -0800

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -5,10 +5,23 @@
  */
 
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./reservations.controller");
 
-router.route("/:reservation_id/status").put(controller.statusUpdate)
-router.route("/:reservationId").get(controller.read).put(controller.put)
-router.route("/").get(controller.list).post(controller.create);
+router
+  .route("/:reservation_id/status")
+  .put(controller.update)
+  .all(methodNotAllowed)
+
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.edit)
+  .all(methodNotAllowed)
+
+router.route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
