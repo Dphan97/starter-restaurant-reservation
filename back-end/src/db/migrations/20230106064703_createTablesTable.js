391976942f9b35ec08c99601ed8c387d80d79bf1
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2023-01-10 13:12:42 -0800

    Update 20230106064703_createTablesTable.js

--- back-end/src/db/migrations/20230106064703_createTablesTable.js
+++ back-end/src/db/migrations/20230106064703_createTablesTable.js
@@ -1,17 +1,21 @@
-exports.up = function (knex) {
-    return knex.schema.createTable("tables", (table) => {
-      table.increments("table_id").primary();
-      table.string("table_name").notNullable();
-      table.integer("capacity").notNullable();
-      table.integer("reservation_id").defaultTo(null);
-      table
-        .foreign("reservation_id")
-        .references("reservation_id")
-        .inTable("reservations");
-      table.timestamps(true, true);
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable("tables");
-  };
+const {table} = require("../connection");
+
+exports.up = function(knex) {
+  return knex.schema.createTable("tables", (table) => {
+    table.increments("table_id").primary().notNullable();
+    table.string("table_name").notNullable();
+    table.integer("capacity").notNullable();
+    table.string("status").notNullable();
+    table.integer("reservation_id").unsigned();
+    table
+      .foreign("reservation_id")
+      .references("reservation_id")
+      .inTable("reservations")
+      .onDelete("SET NULL");
+    table.timestamps(true, true);
+  });
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("tables");
+}
