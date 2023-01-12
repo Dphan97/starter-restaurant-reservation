41c6dafc421601ec94abd201931b4d6f8b00b1dd
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2023-01-12 01:05:41 -0800

    Add files via upload

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { listReservations, listTables } from "../utils/api";
+import { useHistory } from "react-router-dom";
+import {
+  listTables,
+  listReservations,
+  finishTables,
+  cancelReservations,
+} from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import ReservationsComponent from "./ReservationsComponent";
-import ListTables from "./ListTables";
-import useQuery from "../utils/useQuery";
-import { today, next, previous } from "../utils/date-time";
-import { useHistory } from "react-router-dom"
-import moment from "moment";
+import ReservationCardList from "../reservations/ReservationCardList";
+import TableList from "../tables/TableList";
+import DateChange from "./DateChange";
 
 /**
  * Defines the dashboard page.
@@ -14,52 +17,85 @@ import moment from "moment";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard() {
+function Dashboard({ date, setDate }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+
   const [tables, setTables] = useState([]);
-  const query = useQuery();
-  const date = query.get("date") || today();
+  const [tablesError, setTablesError] = useState(null);
 
   const history = useHistory();
 
   useEffect(loadDashboard, [date]);
+
   function loadDashboard() {
     const abortController = new AbortController();
     setReservationsError(null);
+    setTablesError(null);
     listReservations({ date }, abortController.signal)
       .then(setReservations)
       .catch(setReservationsError);
+
+    listTables({}, abortController.signal)
+      .then(setTables)
+      .catch(setTablesError);
+
     return () => abortController.abort();
   }
 
-  useEffect(loadTables, []);
-  function loadTables() {
+  function finishTable(table_id) {
     const abortController = new AbortController();
-    listTables(abortController.signal)
-      .then(setTables)
+
+    const result = window.confirm(
+      "Is this table ready to seat new guests? This cannot be undone."
+    );
+    if (result) {
+      setTablesError(null);
+      finishTables(table_id, abortController.signal)
+        .then(() => {
+          history.push("/");
+        })
+        .catch(setTablesError);
+    }
+
+    return () => abortController.abort();
+  }
+
+  function cancelReservation(reservation_id) {
+    const abortController = new AbortController();
+
+    const result = window.confirm(
+      "Do you want to cancel this reservation? This cannot be undone."
+    );
+    if (result) {
+      setReservationsError(null);
+      cancelReservations(reservation_id, abortController.signal)
+        .then(() => {
+          history.push("/");
+        })
+        .catch(setReservationsError);
+    }
+
     return () => abortController.abort();
   }
 
   return (
     <main>
-      <h2>Dashboard</h2>
+      <h1 className="display-4">Dashboard</h1>
+      <DateChange date={date} setDate={setDate} />
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for {moment(date).format("ddd MMMM Do, YYYY")}</h4>
+        <h4 className="mb-0 mt-4 h4">Reservations for Date {date}</h4>
       </div>
-      <div>
-      {reservations.length !== 0 ? (
-      <ReservationsComponent reservations={reservations} 
-      loadDashboard={loadDashboard}/>) : 
-      (
-          `There are no reservations today`
-        )}
-        </div>
-      <button className="btn btn-secondary" onClick={() => history.push(`/dashboard?date=${previous(date)}`)}>Previous</button>
-      <button className="btn btn-primary ml-1" onClick={() => history.push(`/dashboard?date=${today()}`)}>Today</button>
-      <button className="btn btn-warning ml-1" onClick={() => history.push(`/dashboard?date=${next(date)}`)}>Next</button>
-      <ListTables tables={tables} loadTables={loadTables} loadDashboard={loadDashboard}/>
       <ErrorAlert error={reservationsError} />
+      <ReservationCardList
+        reservations={reservations}
+        cancelReservation={cancelReservation}
+      />
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0 mt-5 h4">Tables</h4>
+      </div>
+      <ErrorAlert error={tablesError} />
+      <TableList tables={tables} finishTable={finishTable} />
     </main>
   );
 }
