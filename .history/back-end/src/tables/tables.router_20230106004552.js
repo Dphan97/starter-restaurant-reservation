2c110914da0eb416e3ad2fa197f1951f4cfa83c3
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2023-01-12 01:52:00 -0800

    Delete .history directory

--- .history/back-end/src/tables/tables.router_20230106004552.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controller = require("./tables.controller")
-
-
-
-router.route("/:table_id/seat").put(controller.put).delete(controller.freeTable)
-router.route("/").get(controller.list).post(controller.post)
-
-module.exports = router
